5625fd5d47205502a24bc0cf5d8b436b8f482734
hj <hj@163.com>
2021-11-04 20:25:57 +0800

    second commit

--- src/main/java/com/hj/git/GitTest.java
+++ src/main/java/com/hj/git/GitTest.java
@@ -3,6 +3,7 @@ package com.hj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 
 }
